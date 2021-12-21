b34e3b342b3de22b6974f8c4d33907fd49f88009
duypham <xduy.pham@gmail.com>
2021-12-21 16:44:02 +0700

    ket qua

--- bai1.js
+++ bai1.js
@@ -22,36 +22,61 @@ const arrays = [
   },
 ];
 
+const newUser = { id: 6, name: "user 6", age: 22, };
 // task 1: Add mới 1 user vào đầu mảng
+[newUser,...arrays];
+arrays.unshift(newUser);
 
 // task 2: Add mới 1 user vào cuối mảng
+[...arrays, newUser];
+arrays.push(newUser);
 
 // task 3: add nhiều user vào mảng
+const users = [{ id: 7, name: "user 7", age: 20, }, { id: 6, name: "user 6", age: 22, }];
+[...arrays, ...users];
 
 // task 4: xoá 1 user ra khỏi mảng
+arrays.filter(f => f.id !== 3);
 
 // task 5: lọc ra các user có age >= 22;
+arrays.filter(f => f.age >= 23);
 
 // task 6: Biến đổi mảng hiện tại thành một mảng mới với field mới là "class" kiểu string
+arrays.map((m) => ({...m, class: 'Pnv2021'}))
 
 // task 7: Tìm ra 1 user có age = 23
+arrays.find(f => f.age === 23);
 
 // task 8: xử dụng vòng lặp for để lấy ra các user có age >= 23
-
+let personas = [];
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  if (element?.age >= 23) {
+    personas.push(element);
+  }
+}
 
 // task 9: Dựa vào mảng thông tin bên trên, và mảng ids bên dưới, lọc ra thông tin các user có id như mảng bên dưới.
+const arrayIds = [1, 3];
+arrays.filter(f => arrayIds.includes(f.id))
 
 // task 10: sắp xếp các user trong mảng trên theo thứ tự tuổi tăng dần
+arrays.sort((a, b) => a.age - b.age);
 
 // task 11: cập nhật thông tin trong mảng trên với field "class" được biến đổi ở task 6 thành kiểu chữ in hoa (upCase)
+arrays.map((m) => ({...m, class: m?.class?.toUpperCase()}))
 
 // task 12: lấy ra phần tử cuối cùng của mảng trên
+arrays.pop();
+arrays[arrays.length - 1];
 
 // task 13: lấy ra phần tử đầu tiên của mảng trên
+arrays[0];
 
 // task 14: cho 2 mảng bên dưới, hợp nhất 2 mảng lại thành một và xoá các phần tử trùng lặp, result mong đợi => ["Gà", "vịt", "Chó"]
 const array1 = ["Gà", "vịt"];
 const array2 = ["vịt", "Chó"];
+const mergedArrays = [... new Set([...array1, ...array2])]
 
 // -----------------------------------
 const person = {
@@ -60,18 +85,28 @@ const person = {
 };
 
 // task 15: từ object person trên lấy ra key của nó. result => ['firstName', 'lastName']
+Object.keys(person);
 
 // task 16: từ object person trên lấy ra value của nó. result => ['John', 'Doe']
+Object.values(person);
 
 // task 17: chuyển đổi object trên thành array. result mong đợi => [{firsName: 'John'}, {lastName: 'Doe'}]
+Object.keys(person).map(key => {
+  return {[key]: person[key]}
+})
 
 // task 18: từ object person trên add thêm 1 field "age" vao object. kết qua mong đợi => { firstName: "John", lastName: "Doe", age: 100 };
+person = {...person, age: 100};
 
 // task 19: cho array string bên dưới, convert array này sang object. Result mong đợi => {0:"a", 1:"b", 2:"c"}
 const arrString = ["a", "b", "c"];
+let newObject = {...arrString};
+newObject = Object.assign({}, arrString);
+newObject = arrString.reduce((a, v) => ({ ...a, [v]: v}), {});
 
 // task 20: cho array boolean bên dưới, kiểm tra output của array này là true hoặc false
 const arrBoolean = [true, true, false];
+arrBoolean.every(s => s === true);
 
 // task 21: viết 1 object sử dụng setter & getter để thêm mới value và lấy ra value. key word (Getter, setter)
 
@@ -83,7 +118,9 @@ const newFunction = (user) => {
 
 // task 23:
 let numb = 1.4; // => làm tròn lên số bên.  result mong đợi = 2
+Math.ceil(numb);
 let numb = 1.4; // => làm tròn lên số bên.  result mong đợi = 1
+Math.round(numb);
 
 // task 24: Thực hiện nối chuỗi dữa vào các biến bên dưới. kết quả mong đợi. => 'name: user - age: 21 - class: classAbc'
 let name = "user";
@@ -92,3 +129,4 @@ let className = "classAbc";
 
 // task 25: cho array number bên dưới, tính tổng array: result = 45;
 const arrNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+arrNumber.reduce((a, b) => a + b);
--- bai2.js
+++ bai2.js
@@ -1,13 +1,67 @@
 // task 1: Cho một chuỗi, Viết hàm đảo ngược từng từ trong câu
+const reverseString = (str) => str.split('').reverse().join("");
 
 // task 2: Viết hàm đệ quy đảo ngược chuỗi.
+const recursiveReverseString = (str) => {
+    if (!str) return true;
+    const isStr = str.charAt(0) === str.charAt(str.length - 1);
+    return isStr && recursiveReverseString(str.slice(1, str.length -1));
+}
 
 // task 3: Cho một mảng, Viết hàm đảo ngược các item trong array đó.
+const reverseArray = (arr) => arr.reverse();
 
 // task 4: viết hàm đệ quy đảo ngược một mảng
+const recursiveReverseArray = (arr) => {
+    if (arr.length === 1 || arr.length === 0) return arr;
+    return [...recursiveReverseArray(arr.slice(1)), arr[0]];
+}
 
 // task 5: viết hàm đệ quy tính tổng
+const recursiveSum = (numbers) => {
+    if (numbers.length === 0) return 0;
+    if (numbers.length === 1) return numbers[0];
+    return numbers[0] + recursiveSum(numbers.slice(1));
+}
 
 // task 6: Viết hàm đệ quy để biết một số là số nguyên tố
+const recursivePrime = (num, i = 3) => {
+    if (num === 2) return true;
+    if (num % 2 === 0 || num < 2 ) return false;
+    if (i * i > num) return true;
+    if (num % i === 0) return false;
+    return recursivePrime(num, i + 2);
+}
 
 // task 7: Viết một class bao gồm 4 methods: add, update, remove, so sánh 
+class classUser {
+    users = [{ id: 1, name: 'user 1' }, { id: 2, name: 'user 2' }];
+
+    add(user) {
+        const isNum = this.checkNum(user.id);
+        if (isNum) return this.users;
+        this.users = [...this.users, user];
+        return this.users;
+    };
+
+    remove(userId) {
+        this.users = this.users.filter(f => f.id !== userId);
+        return this.users;
+    };
+
+    updated(user) {
+        const index = this.users.findIndex(t => t.id === user.id);
+        this.users[index] = user;
+        return this.users;
+    };
+
+    checkNum(userId) {
+        return this.users.some(s => s.id === userId);
+    };
+};
+
+const useClassUser = new classUser();
+
+useClassUser.add({id: 3, name: 'user 3'});
+useClassUser.remove(1);
+useClassUser.updated({id: 2, name: 'USER 2'});
